dce25b341b7e3f3e9e6902bdc15ace445286d76e
jichengyue <ji19910119@163.com>
2013-08-08 14:45:54 +0800

    JCY_BLOG first update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JCYBlog::Application.config.secret_key_base = '4762db02c8124d34b7f129c6a09c6d91509c0bc7bca2a14b13a30a19974f456c0e8850aa9ebaf21fc090621514062b22ac051d541fe395da59684e263497fc4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JCYBlog::Application.config.session_store :cookie_store, key: '_JCY_BLOG_session'
